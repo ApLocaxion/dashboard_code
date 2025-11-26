ef93210467814c87b2e07db295ba61cee33f8b38
ApLocaxion <ajay@locaxion.com>
2025-11-26 23:00:58 +0530

    api address

--- dashboard/lib/utility/constant.dart
+++ dashboard/lib/utility/constant.dart
@@ -1,10 +1,5 @@
 class Constants {
-  // static const webSocketUrl =
-  //     'ws://94.112.79.114/v0/slam/ws/2b6b92fd31a6502d2bf1710efda04bc4225c4376c1';
   static const webSocketUrl = 'ws://localhost:8081';
-  // static const baseApiUrl = '';
-  // static const baseApiUrilocal = 'http://localhost:80';
-
   static const baseApiUrilocal = 'http://localhost:3000';
 
   // static const baseApiUrilocal = 'http://192.168.137.1:3000';
