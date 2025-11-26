47fa992ef0551bc94e8baf6141e92f6e62edf0a8
ApLocaxion <ajay@locaxion.com>
2025-11-26 19:52:39 +0530

    tab dash .,

--- dashboard/build/web/flutter_service_worker.js
+++ dashboard/build/web/flutter_service_worker.js
@@ -39,7 +39,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "49ef2280b198aa6a11ff3e0ca04ce165
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "ceb4e784d13b40d150bef0c1eac3459f",
+"flutter_bootstrap.js": "81a3dd6266afea61cd3937ac7fb2478e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
