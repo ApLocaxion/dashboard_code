abd9dd642361ce4efd4d8e63859e567f1c24d6a4
ApLocaxion <ajay@locaxion.com>
2025-11-26 13:45:38 +0530

    latest2.

--- dashboard/build/web/flutter_bootstrap.js
+++ dashboard/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3128460270"
+    serviceWorkerVersion: "1509833706"
   }
 });
